2525199dbaa537c42bc8bd0e8e463af0474667b8
devjavaee <devjavaee16@gmail.com>
2026-07-16 16:46:59 +0200

    Supabase rétablie

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'irysjvkfatygmlnvfmxy.supabase.co', // Ton ID projet ici
+        hostname: 'kvnamkgpbbxzvlvommdm.supabase.co', // Ton ID projet ici
         port: '',
         pathname: '/storage/v1/object/public/**',
       },
